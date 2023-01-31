3130655eaa5836bba4f2f6a8d36b6210e0ee9e2d
Gonzalo Martinez <gonzalo_m@live.com>
2023-01-31 03:01:53 -0300

    Primer escritura en el repositorio

---
+++ js/funciones.js
@@ -0,0 +1,8 @@
+document.getElementById("botonMail").onclick = function () {
+    document.getElementById("demo1").innerHTML = "gonzalo_m@live.com"
+    
+}
+document.getElementById("botonCel").onclick = function () {
+    document.getElementById("demo").innerHTML = "0351-152221523"
+    
+}
